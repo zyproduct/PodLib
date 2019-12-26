b723401b4cd157948d12ecf9bc057107c1c8ca0c
lzy <linzy30@taikanglife.com>
2019-12-26 14:00:41 +0800

    first commit

---
+++ PodLib/Classes/PodLib.h
@@ -0,0 +1,17 @@
+//
+//  PodLib.h
+//  PodLib
+//
+//  Created by lzy on 2019/12/26.
+//  Copyright © 2019 Creative Knowledge Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PodLib : NSObject
+
++ (void)logSome;
+
+@end
+
+
---
+++ PodLib/Classes/PodLib.m
@@ -0,0 +1,17 @@
+//
+//  PodLib.m
+//  PodLib
+//
+//  Created by lzy on 2019/12/26.
+//  Copyright © 2019 Creative Knowledge Ltd. All rights reserved.
+//
+
+#import "PodLib.h"
+
+@implementation PodLib
+
++ (void)logSome {
+    NSLog(@"there some places i remmber");
+}
+
+@end
